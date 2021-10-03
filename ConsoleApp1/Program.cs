3e2789356ffeeb23709c1f3d2b228bd67e7be192
Fujitsu <Fujitsu@DESKTOP-I97U5MM>
2021-10-04 00:06:35 +0400

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            //int n = 21;
+
+            //if (n % 3 == 0 && n % 7 == 0)
+
+            //{
+            //    Console.WriteLine(true);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(false);
+            //}
+            #endregion
+
+            #region
+            //Task 2.Verilmiş n və m(n < m) ədədləri arasında neçə tək ədəd olduğunu tapın
+            //int n = 10;
+            //int m = 50;
+            //int count = 0;
+            //for (int i = n; i <= m; i++)
+            //{
+            //    if (i % 3 == 0)
+
+            //    {
+
+            //        count++;
+
+            //    }
+            //}
+            //Console.WriteLine(count);
+            #endregion
+
+            #region
+            //Task 3 Verilmiş n tam ədədinin sadə və ya mürəkkəb ədəd olduğunu tapın
+            //int n = 200;
+
+            //for (int i = 0; i <= n; i++)
+            //{
+            //    if (n % n == 0 && n % 1 == 0)
+            //    { Console.WriteLine("sade"); }
+            //    else if (n % 1 == 0 && n % n == 0 && n % i == 0)
+            //    {
+            //        Console.WriteLine("murekkeb");
+            //    }
+            //}
+            #endregion
+
+            #region
+            //Task 4 Verilmiş müsbət tam n ədədinin 2 - nin qüvvəti olub-olmamasını tapın
+
+            //int n = 16;
+
+            //if (n % 2 == 0)
+            //{
+            //    Console.WriteLine(false);
+            //}
+            //else if (n == 1)
+            //{
+            //    Console.WriteLine(true);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(false);
+            //}
+
+            #endregion
+
+            #region
+            //Task 5 Verilmiş n tam ədədinin neçə mərtəbəli olduğunu tapın. Məs: 23452, output: 5
+
+            //int n = 50000;
+            //int count = 0;
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    n /= 10;
+            //    count++;
+            //    if (n / 10 == 0)
+            //    {
+            //        break;
+            //    }
+
+            //}
+            //Console.WriteLine(count + 1);
+
+
+
+            #endregion
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
